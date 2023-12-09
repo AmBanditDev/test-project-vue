e914ee4fc6ae450713f3848006eb34c9a9c15047
BanditDev <thanwabandit.yos@gmail.com>
2023-12-09 15:08:13 +0700

    update content aboutview

--- test-vuejs/src/views/AboutView.vue
+++ test-vuejs/src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>เกี่ยวกับเรา</h1>
     <h3>สวัสดีครับ ผมนายธันวบัณฑิต</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui itaque tenetur id at, voluptatum ea fugit reiciendis nobis, provident ducimus non fuga quidem eveniet aliquid nostrum sit suscipit ratione deserunt.</p>
+    <h3>อายุ 22 ปี</h3>
+    <h3>ระดับการศึกษา: มหาวิทยาลัยราชภัฏสวนสุนันทา สาขาวิทยาการคอมพิวเตอร์</h3>
   </div>
 </template>
